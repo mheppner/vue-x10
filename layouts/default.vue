20fa8ee3468960456ae3fb82b1f46cbaeb522b89
Mark Heppner <mheppner@users.noreply.github.com>
2017-11-20 23:00:47 -0500

    Initial

---
+++ layouts/default.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      clipped
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile nuxt :to="{name: 'index'}">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Units</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile :to="{name: 'scenes'}">
+          <v-list-tile-action>
+            <v-icon>group_work</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Scenes</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Admin</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile :to="{name: 'logout'}">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Log out</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>X10</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <nuxt />
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: true
+    })
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <section class="container">
+
+    <h3 class="headline">Unit name</h3>
+    <v-layout row wrap>
+      <v-flex xs6>
+        <v-btn block
+               color="grey darken-2"
+               :outline="!state"
+               :disabled="disabled"
+               @click="state = true">
+          <v-icon>lightbulb_outline</v-icon>
+          On
+        </v-btn>
+      </v-flex>
+      <v-flex xs6>
+        <v-btn block
+               color="grey darken-3"
+               :outline="state"
+               :disabled="disabled"
+               @click="state = false"
+               :class="pulse ? 'pulse': ''">
+          <v-icon>visibility_off</v-icon>
+          Off
+        </v-btn>
+      </v-flex>
+      <v-flex xs3>
+        <v-btn block
+               small
+               :disabled="disabled">
+          <v-icon>skip_previous</v-icon>
+        </v-btn>
+      </v-flex>
+      <v-flex xs3>
+        <v-btn block
+               small
+               :disabled="disabled">
+          <v-icon>fast_rewind</v-icon>
+        </v-btn>
+      </v-flex>
+      <v-flex xs3>
+        <v-btn block
+               small
+               :disabled="disabled">
+          <v-icon>fast_forward</v-icon>
+        </v-btn>
+      </v-flex>
+      <v-flex xs3>
+        <v-btn block
+               small
+               :disabled="disabled">
+          <v-icon>skip_next</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      state: false,
+      pulse: false
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+</style>

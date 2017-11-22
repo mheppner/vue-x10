37467244ea626523ed8f426f274334219f7ddfa2
Mark Heppner <mheppner@users.noreply.github.com>
2017-11-22 17:26:06 -0500

    Use a websocket URL based on the axios base URL

--- plugins/websocket.js
+++ plugins/websocket.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import VueNativeSock from 'vue-native-websocket'
 
 if (process.browser) {
-  window.onNuxtReady(({$store}) => {
-    Vue.use(VueNativeSock, 'ws://localhost:8000/status/', {
+  window.onNuxtReady(({$store, $axios}) => {
+    const baseUrl = $axios.defaults.baseURL.replace('http', 'ws')
+    const url = `${baseUrl}status/`
+
+    Vue.use(VueNativeSock, url, {
       format: 'json',
       store: $store,
       reconnection: true,
